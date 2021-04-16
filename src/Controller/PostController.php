d6d811307fe7fa4a9f3cb7ac00495e238eb26375
Marwane Benharoun <71384548+MBenharoun@users.noreply.github.com>
2021-04-16 16:42:44 +0200

    Post Routing

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -15,27 +15,128 @@ class PostController extends AbstractController
     public function index(): Response
     {
         $array =
-        [
-            "Raphael",
-            "Robin",
-            "Antoine",
-            "Nicolas",
-            "Tom"
-        ];
+            [
+                "Raphael",
+                "Robin",
+                "Antoine",
+                "Nicolas",
+                "Tom"
+            ];
 
         $mesPosts =
-        [
-            new Post(1, 1 , "Hello PHP", "On fait du Symfony"),
-            new Post(2, 1 , "Hello C#", "On fait du C#"),
-            new Post(3, 1 , "Hello JS", "On fait du JS"),
-            new Post(4, 1 , "Hello", "On fait du CodeCombat"),
-        ];
+            [
+                new Post(1, 1, "Hello PHP", "On fait du Symfony"),
+                new Post(2, 1, "Hello C#", "On fait du C#"),
+                new Post(3, 1, "Hello JS", "On fait du JS"),
+                new Post(4, 1, "Hello", "On fait du CodeCombat"),
+            ];
 
         return $this->render('post/index.html.twig',
-        [
-            'controller_name' => 'PostController',
-            'personnes' => $array,
-            'mesPosts' => $mesPosts
-        ]);
+            [
+                'controller_name' => 'PostController',
+                'personnes' => $array,
+                'mesPosts' => $mesPosts
+            ]);
+    }
+        /**
+         * @Route("/post/1", name="post1")
+         */
+        public
+        function post1(): Response
+        {
+            $array =
+                [
+                    "Raphael",
+                    "Robin",
+                    "Antoine",
+                    "Nicolas",
+                    "Tom"
+                ];
+
+            $mesPosts =
+                    new Post(1, 1, "Hello PHP", "On fait du Symfony");
+
+            return $this->render('post/index.html.twig',
+                [
+                    'controller_name' => 'PostController',
+                    'personnes' => $array,
+                    'mesPosts' => $mesPosts
+                ]);
+        }
+    /**
+     * @Route("/post/2", name="post2")
+     */
+    public
+    function post2(): Response
+    {
+        $array =
+            [
+                "Raphael",
+                "Robin",
+                "Antoine",
+                "Nicolas",
+                "Tom"
+            ];
+
+        $mesPosts = new Post(2, 1, "Hello C#", "On fait du C#");
+
+
+        return $this->render('post/index.html.twig',
+            [
+                'controller_name' => 'PostController',
+                'personnes' => $array,
+                'mesPosts' => $mesPosts
+            ]);
+    }
+    /**
+     * @Route("/post/3", name="post3")
+     */
+    public
+    function post3(): Response
+    {
+        $array =
+            [
+                "Raphael",
+                "Robin",
+                "Antoine",
+                "Nicolas",
+                "Tom"
+            ];
+
+        $mesPosts =
+            new Post(3, 1, "Hello JS", "On fait du JS");
+
+        return $this->render('post/index.html.twig',
+            [
+                'controller_name' => 'PostController',
+                'personnes' => $array,
+                'mesPosts' => $mesPosts
+            ]);
+    }
+    /**
+     * @Route("/post/4", name="post4")
+     */
+    public
+    function post4(): Response
+    {
+        $array =
+            [
+                "Raphael",
+                "Robin",
+                "Antoine",
+                "Nicolas",
+                "Tom"
+            ];
+
+        $mesPosts =
+
+                new Post(4, 1, "Hello", "On fait du CodeCombat");
+
+        return $this->render('post/index.html.twig',
+            [
+                'controller_name' => 'PostController',
+                'personnes' => $array,
+                'mesPosts' => $mesPosts
+            ]);
     }
 }
